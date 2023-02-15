5fadbbca0cf36ebbe6ed0c3251e91305d779a9e8
dorkospron0 <tareassec9@gmail.com>
2023-02-14 23:16:12 -0500

    cambios hechos

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +7,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  created(){
+    axios.get('http://localhost:3000/productos')
+      .then(resp => console.log(resp.data))
+      .catch(err => console.error(err))
   }
 }
 </script>
