f676f7b9eb88c99a09c8b90bfa501faa0ae390fd
MCKENZY69 <thebysebas0987.monsal@gmail.com>
2023-02-14 22:31:41 -0500

    cambios minimos porque nicky es un tocado hpta

--- Rest-API/src/index.js
+++ Rest-API/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('json space', 2)
@@ -9,10 +10,11 @@ app.set('json space', 2)
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(require('./routes/index.routes'));
+app.use(cors());
+
 // routers
-
-
+app.use(require('./routes/index.routes'));
+app.use("/productos", require("./routes/r_productos")); 
 //start the server
 app.listen(app.get('port'), () => {
     console.log(`Server On port ${app.get('port')}`);
--- src/routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-
-router.get(('/'), (req, res) => {
-    res.json({'title': 'Hello HIJO DE LA GRAN PUTA MALDITA PUELCA'})
-});
-
-module.exports = router;
